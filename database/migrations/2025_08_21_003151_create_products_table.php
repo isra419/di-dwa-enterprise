c8a7f2341cdc9fc8dbc6215b342771899eed1e03
Keys <keys14052002@gmail.com>
2025-08-21 03:03:24 +0000

    chore: set up database migrations

---
+++ database/migrations/2025_08_21_003151_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('name');
+            $table->decimal('rating', 1, 1);
+            $table->string('product_image', 1000);
+            $table->string('price')->default(0);
+            $table->unsignedTinyInteger('discount');
+            $table->integer('qty_in_stock')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
